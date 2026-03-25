d7b95672f1dc0a1432c319b0e61bfdef5a12409f
gabrielal4k <162071635+gabrielal4k@users.noreply.github.com>
2026-03-25 22:25:44 +0800

    templates mud, fix the stupid corsdll error

--- BLAZORBASIC/TodoApp/TodoApp.Portal/Program.cs
+++ BLAZORBASIC/TodoApp/TodoApp.Portal/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using TodoApp.Portal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //builder.HostEnvironment.BaseAddress = "https://localhost:4000";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:4000") });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
